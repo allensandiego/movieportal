b78f79d47a8a7f2b6b4368448d69bf60f72e500a
Rinoa Heartlilly <rinoa.heartlilly@aensoft.com>
2026-05-28 11:38:25 +0000

    build: increment versionCode and versionName for Google Play deployment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.allensandiego.movieportal"
         minSdk = 30
         targetSdk = 36
-        versionCode = 30
-        versionName = "2.0.0"
+        versionCode = 31
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
